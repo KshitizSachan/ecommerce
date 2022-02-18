72c4b5e018b3865320501310ba49d274da1b7193
KshitizSachan <97247354+KshitizSachan@users.noreply.github.com>
2022-02-18 22:18:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const bar= document.getElementById('bar');
+
+const nav= document.getElementById('navbar');
+
+if(bar){
+    bar.addEventListener('click',()=>{
+        nav.classList.add('active');
+    })
+}
+/*
+const close= document.getElementById('close');
+
+if(close){
+    bar.addEventListener('click',()=>{
+        nav.classList.remove('active');
+    })
+}
+*/
+
+
+
+
+
+
+
+
